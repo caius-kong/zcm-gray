290b5b509a5dd41b77612016827d8ed94588316d
孔昀晖 <1032316751@qq.com>
2018-08-16 20:20:55 +0800

    dubbo test app1

--- gray-dubbo-test/src/main/java/com/kyh/test1/MyController.java
+++ gray-dubbo-test/src/main/java/com/kyh/test1/MyController.java
@@ -13,12 +13,12 @@ import com.kyh.api.DemoService;
  * @author kongyunhui on 2018/8/15.
  */
 @RestController
-@RequestMapping("app1")
+@RequestMapping("/")
 public class MyController {
     @Autowired
     private DemoService demoService;
 
-    @RequestMapping("/")
+    @RequestMapping("dubboCaller")
     public ResponseEntity<String> dubboCaller() {
 //        RpcContext.getContext().setAttachment("clientIP", "2");
         String str = demoService.sayHello("kong");
